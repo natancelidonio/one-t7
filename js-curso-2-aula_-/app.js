a2f44b51797ba69257802f7a930ad251c34d64f0
natancelidonio <natan.c.f.adm@gmail.com>
2024-06-22 16:46:15 -0300

    Lógica de programação: explore funções e listas 2/2

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -4,6 +4,8 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,6 +13,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 };
 
 function exibirMensagemInicial() {
@@ -46,7 +49,18 @@ function verificarChute() {
 
 // função sem parâmetro e com retorno
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 +1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite +1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    };
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido)
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    };
 };
 
 function limparCampo() {
---
+++ js-curso-2-aula_-/lista-de-atividades/aula04-listas.js
@@ -0,0 +1,26 @@
+// 1.
+let listaGenerica = [];
+
+// ################################################
+// 2.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// ################################################
+// 3.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// ################################################
+// 4.
+let listaNomes = ['João', 'Francisco', 'Maria'];
+console.log(listaNomes[0]);
+
+// ################################################
+// 5.
+let listaNomes2 = ['João', 'Francisco', 'Maria'];
+console.log(listaNomes2[1]);
+
+
+// ################################################
+// 6.
+let listaNomes3 = ['João', 'Francisco', 'Maria'];
+console.log(listaNomes3[2]);
